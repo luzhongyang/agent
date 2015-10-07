557c1ed6acdb37dfe6c9fdff24832bc2d575ae2f
luzhongyang <914577188@qq.com>
2015-10-07 12:39:37 +0800

    init

---
+++ demo/index.php
@@ -0,0 +1,8 @@
+<?php
+
+include('../src/Agent.php');
+
+$agent = new \Agent\Agent();
+
+var_dump($agent->isMobile());
+
---
+++ src/Agent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Agent;
+
+class Agent
+{
+    public function isMobile()
+    {
+        return (boolean)preg_match(
+            '/(up.browser|up.link|mmp|symbian|smartphone|midp|wap|phone|iphone|ipad|ipod|android|xoom)/i',
+            strtolower($_SERVER['HTTP_USER_AGENT'])
+        );
+    }
+}
